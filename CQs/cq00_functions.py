85dc0931a6a718361c2775a92d789b5dfeb87321
Gregorio Guerrero <ggue@unc.edu>
2024-08-28 22:52:11 -0400

    Finished Challenge Question 00!

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""My first Challenge Question in Comp110!"""
+
+__author__ = "730812817"
+
+
+def mimic(message: str) -> str:
+    """Returns the message."""
+    return message
+
+
+def main() -> None:
+    """Prints the result of calling the function mimic."""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
