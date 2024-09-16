b2a8272528a02e6f7471e085c65a8489d1246a85
Gregorio Guerrero <ggue@unc.edu>
2024-09-15 20:02:19 -0400

    Completed CQ02 and fixed slight error in EX01.

---
+++ CQs/cq02_conditionals.py
@@ -0,0 +1,19 @@
+"""A simple number guessing game."""
+
+__author__ = "730812817"
+
+
+def guess_a_number() -> None:
+    secret: int = 7
+    number_guessed: int = int(input("Guess a number: "))
+    print("Your guess was ", number_guessed)
+    if number_guessed == secret:
+        print("You got it!")
+    elif number_guessed < secret:
+        print("Your guess was too low! The secret number is ", secret)
+    else:
+        print("Your guess was too high! The secret number is ", secret)
+
+
+if __name__ == "__main__":
+    guess_a_number()
--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -31,6 +31,6 @@ def cost(tea_count: int, treat_count: int) -> float:
     return (tea_count * 0.50) + (treat_count * 0.75)
 
 
-# Have to place below functions because they haven't been defined yet at the top of the program
+# Placed below functions as they haven't been defined yet at the top of the program
 if __name__ == "__main__":
     main_planner(guests=int(input("How many guests are attending your tea party? ")))
