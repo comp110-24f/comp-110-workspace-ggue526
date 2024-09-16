46d1497182153b836ac2345b09f555938b9d4a45
Gregorio Guerrero <ggue@unc.edu>
2024-09-15 20:14:08 -0400

    Fixed Output for CQ02.

--- CQs/cq02_conditionals.py
+++ CQs/cq02_conditionals.py
@@ -6,13 +6,13 @@ __author__ = "730812817"
 def guess_a_number() -> None:
     secret: int = 7
     number_guessed: int = int(input("Guess a number: "))
-    print("Your guess was ", number_guessed)
+    print("Your guess was", number_guessed)
     if number_guessed == secret:
         print("You got it!")
     elif number_guessed < secret:
-        print("Your guess was too low! The secret number is ", secret)
+        print("Your guess was too low! The secret number is", secret)
     else:
-        print("Your guess was too high! The secret number is ", secret)
+        print("Your guess was too high! The secret number is", secret)
 
 
 if __name__ == "__main__":
