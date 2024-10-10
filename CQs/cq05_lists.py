ba2c94f82284e2241ca364edc7729342bd14bc9f
Gregorio Guerrero <ggue@unc.edu>
2024-10-10 19:51:10 -0400

    Completed CQ05.

---
+++ CQs/cq05_lists.py
@@ -0,0 +1,24 @@
+"""Mutating functions."""
+
+__author__ = "730812817"
+
+
+def manual_append(the_list: list[int], num: int) -> None:
+    the_list.append(num)
+
+
+def double(the_list: list[int]) -> list[int]:
+    count: int = 0
+    while count < len(the_list):
+        the_list[count] *= 2
+        count += 1
+    return the_list
+
+
+list_1: list[int] = [1, 2, 3]
+list_2: list[int] = list_1
+
+double(list_2)
+
+print(list_1)
+print(list_2)
